7a33e24291123dab5e8fbb3f8f519041605e3116
iut <75590158+iutparis@users.noreply.github.com>
2021-10-07 20:52:33 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+let menuOpen = false;
+
+function toggleActive(e){
+  let children = e.parentNode.children;
+  if (!menuOpen) {
+    e.className += " active";
+    for (let i = 1; i < children.length; i++) {
+      children[i].style.transitionDelay = `${(7 - i) / 4}s`;
+    }
+  }
+  else {
+    e.className = e.className.replace(" active", "");
+    for (let i = 1; i < children.length; i++) {
+      children[i].style.transitionDelay = `0s`;
+    }
+  }
+  menuOpen = !menuOpen;
+}
+
+function changeColorMode(mode) {
+  document.getElementsByTagName('body')[0].className = mode;
+}
+
+
